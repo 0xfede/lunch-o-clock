57372cb584af6cf7eaa5159c8b611ab58fef38bf
Marco Amadori <mamadori@vivocha.com>
2014-06-18 12:57:23 +0200

    fixes to work with new json places structure

--- public/js/controllers/placeCtrl.js
+++ public/js/controllers/placeCtrl.js
@@ -1,33 +1,29 @@
 angular.module('lunchApp')
 .controller('placeCtrl', ['$scope','$route','places', function ($scope,$route,places) {
 
+	var u = 'user0';
+
 	$scope.place = {};
 
 	$scope.toggleVote = function(item){
+		var idx = item.indexOf(u);
+		if (idx < 0) item.push(u);
+		else item.splice(idx,1);
+	}
 
-		if (item.voted){
-			delete item.voted;
-			item.count--;
-		}
-		else {
-			item.voted = true;
-			item.count++;
-		}
+	$scope.voted = function(item){
+		return item.indexOf(u) != -1;
 	}
 
 	$scope.pro = function(){
 		var total = 0;
-		for (var i in $scope.place.prolist){
-			total += $scope.place.prolist[i].count;
-		}
+		for (var i in $scope.place.pros) total += $scope.place.pros[i].length;
 		return total;
 	}
 
 	$scope.cons = function(){
 		var total = 0;
-		for (var i in $scope.place.conslist){
-			total += $scope.place.conslist[i].count;
-		}
+		for (var i in $scope.place.cons) total += $scope.place.cons[i].length;
 		return total;
 	}
 
--- public/js/controllers/placesCtrl.js
+++ public/js/controllers/placesCtrl.js
@@ -1,12 +1,30 @@
 angular.module('lunchApp')
 .controller('placesCtrl', ['$scope','places', function ($scope,places) {
 	
-	$scope.radioModel = "pro";
+	$scope.radioModel = "pros";
 	$scope.orderDir = true;
+	$scope.places = [];
 
-	//$scope.places = places.getPlaces();
+	var prosCount = function(place){
+		var t = 0;
+		for (var i in place.pros) t += place.pros[i].length;
+		return t;
+	};
+
+	var consCount = function(place){
+		var t = 0;
+		for (var i in place.cons) t += place.cons[i].length;
+		return t;
+	};
+
 	places.getPlaces().success(function(data){
-		$scope.places = data;
+
+		angular.forEach(data, function(place){
+			place.pros = prosCount(place);
+			place.cons = consCount(place);
+			$scope.places.push(place);
+		});
+		
 	});
 
 }])
